05ec7b741af9b762f90390768467cbd360d76ebf
Cristian Brinza <cristian.brinza@isa.utm.md>
2023-04-27 10:36:29 +0300

    update

--- src/projects/onthemap/pages/home/HomePage.tsx
+++ src/projects/onthemap/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Container, Form, Button, Alert } from 'react-bootstrap';
@@ -7,17 +7,19 @@ import { useTranslation } from 'react-i18next';
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 
-interface User {
-    email: string;
-    password: string;
-}
 const OTMhomePage: React.FC = () => {
 
     const { t, i18n } = useTranslation('home');
 
 
 
+    const [title, setTitle] = useState("On the Map");
+    //'/images/onthemap/logo_black.png'
+    useEffect(() => {
+        // This will run when the page first loads and whenever the title changes
+        document.title = title;
 
+    }, [title]);
 
 
 
